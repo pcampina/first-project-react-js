28c4def6a830d3b0ba384c39ea6b5958375018a0
pcampina <pablo.dsn@gmail.com>
2020-05-11 19:42:04 -0300

    Finished design

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/62779976?s=460&u=46bcb655c35c9650447dde60440638369ca9d3f9&v=4"
+            alt="Pablo Campina"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/62779976?s=460&u=46bcb655c35c9650447dde60440638369ca9d3f9&v=4"
+            alt="Pablo Campina"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/62779976?s=460&u=46bcb655c35c9650447dde60440638369ca9d3f9&v=4"
+            alt="Pablo Campina"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
